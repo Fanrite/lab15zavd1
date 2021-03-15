b1bac6c816d5fc659f3bd9cc32c96817ffb33577
Fanrite <76695059+Fanrite@users.noreply.github.com>
2021-03-16 01:20:18 +0200

    fix

---
+++ zavd1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zavd1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s, outp = "eror";
+            char o = 'о';
+            int i = 0;
+
+            Console.Write("vvedit stroku:");
+            s = Console.ReadLine();
+
+            foreach (char f in s)
+            {
+                if (o == f)
+                    i++;
+            }
+
+            if (i == 0)
+                outp = "bukvi o nemae";
+
+            else if (i != 0)
+                outp = $"tut {i} bukv o";
+
+            Console.WriteLine($"{outp}");
+            Console.ReadKey();
+        }
+    }
+}
